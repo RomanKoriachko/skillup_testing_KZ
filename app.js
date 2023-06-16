135c9d27f2d4393f4967ce008517701cd32a604a
Roman <mazaxaka.tyt@gmail.com>
2023-06-16 18:08:13 +0300

    translate all text

--- app.js
+++ app.js
@@ -190,3 +190,15 @@ $(document).ready(function () {
         autoplaySpeed: 1000,
     });
 });
+
+// Disable hover on mobile
+
+if (
+    "ontouchstart" in window ||
+    (window.DocumentTouch && document instanceof DocumentTouch)
+) {
+    // console.log("this is a touch device");
+} else {
+    // console.log("this is not a touch device");
+    document.body.classList.add("no-touch");
+}
